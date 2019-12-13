b36113db1be580a4bbe468e19bac7306bc1940ca
Tomasz Zajac <ktzajac@poczta.onet.pl>
2019-12-13 11:18:12 +0100

    I went throught all the course

---
+++ 08-Milestone Project - 2/cap.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  7 20:44:19 2019
+
+@author: ktzaj
+"""
+def cap_text(text):
+    return text.title()
---
+++ 08-Milestone Project - 2/test_cap.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  7 20:45:05 2019
+
+@author: ktzaj
+"""
+
+import unittest
+import cap
+
+class TestCap(unittest.TestCase):
+
+    def test_one_word(self):
+        text = 'python'
+        result = cap.cap_text(text)
+        self.assertEqual(result, 'Python')
+
+    def test_multiple_words(self):
+        text = 'monty python'
+        result = cap.cap_text(text)
+        self.assertEqual(result, 'Monty Python')
+
+if __name__ == '__main__':
+    unittest.main()
